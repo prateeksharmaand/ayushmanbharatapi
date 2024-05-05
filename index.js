d3836d5dc146a5a1c86f1fa5998d55757660d726
prateeksharmaad <prateek.sharma@kalagato.co>
2024-05-05 19:08:10 +0530

    added

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const routes = require('./routes/routes');
 app.use('/api', routes)
 
 
-app.listen(port = process.env.PORT||8080, () => {
+app.listen(port = process.env.PORT, () => {
    
 
     console.log(`Server Started at ${3001}`)
