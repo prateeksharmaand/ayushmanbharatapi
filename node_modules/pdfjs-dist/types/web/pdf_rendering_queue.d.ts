8b0a7773c74f340325f4230f8ad65e8c7085f039
grgov <prateeksharmaand@gmail.com>
2025-02-07 12:37:40 +0530

    aaa

--- node_modules/pdfjs-dist/types/web/pdf_rendering_queue.d.ts
+++ node_modules/pdfjs-dist/types/web/pdf_rendering_queue.d.ts
@@ -27,10 +27,6 @@ export class PDFRenderingQueue {
      */
     isHighestPriority(view: IRenderableView): boolean;
     /**
-     * @returns {boolean}
-     */
-    hasViewer(): boolean;
-    /**
      * @param {Object} currentlyVisiblePages
      */
     renderHighestPriority(currentlyVisiblePages: Object): void;
@@ -40,7 +36,7 @@ export class PDFRenderingQueue {
      * @param {boolean} scrolledDown
      * @param {boolean} [preRenderExtra]
      */
-    getHighestPriority(visible: Object, views: any[], scrolledDown: boolean, preRenderExtra?: boolean | undefined): any;
+    getHighestPriority(visible: Object, views: any[], scrolledDown: boolean, preRenderExtra?: boolean): any;
     /**
      * @param {IRenderableView} view
      * @returns {boolean}
